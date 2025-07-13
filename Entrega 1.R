bef8fb01e84a4a2afde80bbe5e170814dd51b679
Andresruizrodr <andres.ruiz@umag.cl>
2025-07-13 17:19:06 -0300

    Subir

---
+++ Entrega 1.R
@@ -0,0 +1,20 @@
+install.packages("usethis")
+
+usethis::use_git_config(user.name = "AndresRuiz",
+                        user.mail = "Andres.ruiz@umag.cl")
+
+
+usethis::use_git_config(user.name = "Andresruizrodr", 
+                        user.email = "andres.ruiz@umag.cl")
+
+
+usethis::use_git()
+
+usethis::use_github()
+
+usethis::git_sitrep()
+
+
+###
+
+renv::init()
